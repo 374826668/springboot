fd4249f1b9a120c32cd7d8dc4d69dbb0554e4fcf
374826668 <mrlino1@163.com>
2018-09-18 20:34:40 +0800

    properties && log4j2 update 20180918_2034

--- src/main/java/com/springboot/springboot/restController/HelloController.java
+++ src/main/java/com/springboot/springboot/restController/HelloController.java
@@ -1,6 +1,8 @@
 package com.springboot.springboot.restController;
 
 import com.springboot.springboot.bean.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,12 +17,14 @@ import java.util.Map;
 @RestController
 @RequestMapping(value = "/hello")
 public class HelloController {
+    private Logger logger=LoggerFactory.getLogger(HelloController.class);
 
     @Value("${server.port}")
     private String port;
 
     @RequestMapping("/hello")
     public String hello() {
+        logger.info("测试日志");
         return port;
 //        return "Hello Spring Boot!";
     }
